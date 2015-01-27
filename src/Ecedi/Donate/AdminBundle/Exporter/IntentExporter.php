e7e4853c14f10dd36e10bc7db049ca8cbce77fe8
syg <sgogel@ecedi.fr>
2015-01-27 15:49:02 +0100

    update IntentExporter to export affectation code

--- src/Ecedi/Donate/AdminBundle/Exporter/IntentExporter.php
+++ src/Ecedi/Donate/AdminBundle/Exporter/IntentExporter.php
@@ -3,7 +3,7 @@ namespace Ecedi\Donate\AdminBundle\Exporter;
 
 use Doctrine\ORM\QueryBuilder;
 use Doctrine\ORM\Query;
-use Symfony\Bundle\FrameworkBundle\Translation\Translator;
+use Symfony\Component\Translation\TranslatorInterface;
 
 /**
  * Export
@@ -19,7 +19,7 @@ class IntentExporter
 
     private $translator;
 
-    public function __construct(Translator $translator)
+    public function __construct(TranslatorInterface $translator)
     {
         $this->translator = $translator;
     }
@@ -107,6 +107,7 @@ class IntentExporter
             utf8_decode('Méthode de paiement'),
             utf8_decode('Reçu fiscal'),
             'Campagne',
+            'Affectation',
             utf8_decode("Civilité"),
             "Nom",
             utf8_decode("Prénom"),
@@ -165,6 +166,7 @@ class IntentExporter
                     utf8_decode($intent['paymentMethod']),
                     utf8_decode($fiscalreceipt),
                     utf8_decode($intent['campaign']),
+                    isset($intent['affectationCode']) ? utf8_decode($intent['affectationCode']) : '',
                     isset($intent['customer']['civility']) ? utf8_decode($intent['customer']['civility']) : '',
                     isset($intent['customer']['firstName']) ? utf8_decode($intent['customer']['firstName']) : '',
                     isset($intent['customer']['lastName']) ? utf8_decode($intent['customer']['lastName']) : '',
