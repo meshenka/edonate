be378bcfb3eba5f12210a6c959fc9d685abb5489
Sylvain Gogel <sgogel@ecedi.fr>
2016-07-08 18:01:01 +0200

    (fix) cs AmountType

--- src/Ecedi/Donate/FrontBundle/Form/Type/AmountType.php
+++ src/Ecedi/Donate/FrontBundle/Form/Type/AmountType.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * @author Sylvain Gogel <sgogel@ecedi.fr>
+ * @copyright Agence Ecedi (c) 2016
+ * @license http://opensource.org/licenses/MIT MIT
+ */
 namespace Ecedi\Donate\FrontBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
@@ -24,26 +29,29 @@ class AmountType extends AbstractType
     //TODO find a way to remove @translator
     /**
      * @since 2.4 flip keys and values and add choices_as_values option
-     * @param  FormBuilderInterface $builder [description]
-     * @param  array                $options [description]
-     * @return [type]               [description]
+     *
+     * @param FormBuilderInterface $builder [description]
+     * @param array                $options [description]
+     *
+     * @return [type] [description]
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         // Ajout d'un champ de saisi manuel si voulu
         $options['choices']['Other amount'] = 'manual';
+
         $builder
             ->addViewTransformer(new AmountChoiceToIntentAmountTransformer([
                 'manual',
                 'preselected',
             ]))
             ->add('preselected', ChoiceType::class, [
-                'choices'   => $options['choices'],
-                'required'  => false,
-                'expanded'    => true,
-                'multiple'    => false,
-                'label'    => false,
-                'data'        => $options['default'],
+                'choices' => $options['choices'],
+                'required' => false,
+                'expanded' => true,
+                'multiple' => false,
+                'label' => false,
+                'data' => $options['default'],
                 //'placeholder' => false,
                 'choices_as_values' => true,
                 'choice_value' => function ($choice) {
@@ -52,25 +60,25 @@ class AmountType extends AbstractType
                 'choice_translation_domain' => false,
             ])
             ->add('manual', MoneyType::class, [
-                'currency'    => 'EUR',
-                'required'  => false,
-                'label'    => 'amount.other',
+                'currency' => 'EUR',
+                'required' => false,
+                'label' => 'amount.other',
                 'scale' => 0,
                 'constraints' => [
                     new Assert\Range(
                         [
-                          'min'        => $options['min_amount'],
-                          'max'        => $options['max_amount'],
-                          'minMessage'    => $this->translator->trans('amount.min', ['%amount%' => $options['min_amount']], 'validators'),
-                          'maxMessage'    => $this->translator->trans('amount.max', [ '%amount%' => $options['max_amount']], 'validators'),
+                          'min' => $options['min_amount'],
+                          'max' => $options['max_amount'],
+                          'minMessage' => $this->translator->trans('amount.min', ['%amount%' => $options['min_amount']], 'validators'),
+                          'maxMessage' => $this->translator->trans('amount.max', ['%amount%' => $options['max_amount']], 'validators'),
                         ]
                     ),
-                ]
+                ],
             ]
         );
     }
 
-        /**
+    /**
      * {@inheritdoc}
      */
     public function buildView(FormView $view, FormInterface $form, array $options)
@@ -79,16 +87,17 @@ class AmountType extends AbstractType
     }
     /**
      * {@inheritdoc}
+     *
      * @since 2.4 use new method signature since sf 2.7
      */
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'choices'        => [],
-            'min_amount'     => 5,
-            'max_amount'     => 4000,
-            'default'        => '',
-            'title'          => '',
+            'choices' => [],
+            'min_amount' => 5,
+            'max_amount' => 4000,
+            'default' => '',
+            'title' => '',
         ]);
     }
 }
